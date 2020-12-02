267c89138176f2ad447363c211be401adebae057
blaker814 <rab0196@yahoo.com>
2020-12-02 10:45:35 -0600

    Added rolodex

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Heist_2
 {
@@ -6,7 +7,45 @@ namespace Heist_2
     {
         static void Main(string[] args)
         {
-            
+            List<IRobber> rolodex = new List<IRobber>()
+            {
+                new Hacker
+                {
+                    Name = "Tim",
+                    SkillLevel = 35,
+                    PercentageCut = 60,
+                },
+                new Hacker
+                {
+                    Name = "BobPete",
+                    SkillLevel = 55,
+                    PercentageCut = 10,
+                },
+                new Muscle
+                {
+                    Name = "Sara",
+                    SkillLevel = 45,
+                    PercentageCut = 25,
+                },
+                new Muscle
+                {
+                    Name = "BobJoe",
+                    SkillLevel = 55,
+                    PercentageCut = 10,
+                },
+                new LockSpecialist
+                {
+                    Name = "Gus",
+                    SkillLevel = 25,
+                    PercentageCut = 30,
+                },
+                new LockSpecialist
+                {
+                    Name = "BobBob",
+                    SkillLevel = 55,
+                    PercentageCut = 10,
+                },
+            };
         }
     }
 }
