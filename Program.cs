ccf5e9099251c6fd6ad69e5084bd469f045a1426
Tristan Wyatt <69435758+tristan0508@users.noreply.github.com>
2020-12-02 12:40:44 -0500

    User can add to rolodex and create new crew members

--- Program.cs
+++ Program.cs
@@ -44,8 +44,117 @@ namespace Heist_2
                     Name = "BobBob",
                     SkillLevel = 55,
                     PercentageCut = 10,
-                },
+                }
             };
+            
+
+            while(true)
+            {
+
+                Console.WriteLine(rolodex.Count);
+                Console.Write("Enter crew member's name: ");
+                string name = Console.ReadLine();
+                if(name == "")
+                {
+                    break;
+                }
+
+                int speciality = 0;
+                while(true)
+                {
+                    Console.WriteLine(@"
+1. Hacker (Disables alarms)
+2. Muscle (Disarms guards)
+3. Lock Specialist (Cracks vault)
+                    ");
+                    try
+                    {
+                        speciality = int.Parse(Console.ReadLine());
+                        if(speciality > 0 && speciality < 4)
+                        {
+
+                            break;
+                        }
+                    
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("Enter a number 1-3");
+                    }
+                
+                }
+
+                int skill = 0;
+                while(true)
+                {
+                    Console.Write("Enter crew member's skill (1-100): ");
+                    try
+                    {
+                        skill = int.Parse(Console.ReadLine());
+                        if(skill > 0 && skill <= 100)
+                        {
+                            break;
+                        }
+                    
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("Enter a number 1-100");
+                    }
+                
+                }
+
+                int percent = 0;
+                while(true)
+                {
+                    Console.Write("Enter crew member's percentage cut (1-100): ");
+                    try
+                    {
+                        percent = int.Parse(Console.ReadLine());
+                        if(percent > 0 && percent <= 100)
+                        {
+                            break;
+                        }
+                    
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("Enter a number 1-100");
+                    }
+                
+                }
+        
+                switch(speciality)
+                {
+                    case 1:
+                        var Hacker = new Hacker
+                        {
+                            Name = name,
+                            SkillLevel = skill,
+                            PercentageCut = percent
+                        };
+                        rolodex.Add(Hacker);
+                        break;
+                    case 2:
+                        var Muscle = new Muscle
+                        {
+                            Name = name,
+                            SkillLevel = skill,
+                            PercentageCut = percent
+                        };
+                        rolodex.Add(Muscle);
+                        break;
+                    case 3:
+                        var LockSpecialist = new LockSpecialist
+                        {
+                            Name = name,
+                            SkillLevel = skill,
+                            PercentageCut = percent
+                        };
+                        rolodex.Add(LockSpecialist);
+                        break;
+                }
+            }
         }
     }
 }
